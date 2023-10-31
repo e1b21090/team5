d83844a52bc91584419c10f5c2d3f6cb6430b606
Yuzuto Imai <e1b21012@oit.ac.jp>
2023-10-31 16:00:51 +0900

    index.htmlの作成&認証処理

---
+++ src/main/java/team5/game/security/ZinrouAuthConfiguration.java
@@ -0,0 +1,70 @@
+package team5.game.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class ZinrouAuthConfiguration {
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   *
+   * @param http
+   * @return
+   * @throws Exception
+   */
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/entry/**"))
+            .authenticated() // /sample3/以下は認証済みであること
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
+            .permitAll())// 上記以外は全員アクセス可能
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))// h2-console用にCSRF対策を無効化
+        .headers(headers -> headers
+            .frameOptions(frameOptions -> frameOptions
+                .sameOrigin()));
+    return http.build();
+  }
+
+  /**
+   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
+   *
+   * @return
+   */
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    // ユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
+    // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
+    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
+    // $ sshrun htpasswd -nbBC 10 user1 p@ss
+
+    UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$10$8PubbmvtsXaoe7uoiV7Ne.dUD5CmX6C2xZ2Jn8ohJ15crmRgSQ2XS").roles("USER").build();
+    UserDetails user2 = User.withUsername("user2")
+        .password("{bcrypt}$2y$10$8PubbmvtsXaoe7uoiV7Ne.dUD5CmX6C2xZ2Jn8ohJ15crmRgSQ2XS").roles("USER").build();
+    UserDetails user3 = User.withUsername("user3")
+        .password("{bcrypt}$2y$10$8PubbmvtsXaoe7uoiV7Ne.dUD5CmX6C2xZ2Jn8ohJ15crmRgSQ2XS").roles("USER").build();
+    UserDetails user4 = User.withUsername("user4")
+        .password("{bcrypt}$2y$10$8PubbmvtsXaoe7uoiV7Ne.dUD5CmX6C2xZ2Jn8ohJ15crmRgSQ2XS").roles("USER").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(user1, user2, user3, user4);
+  }
+
+}
