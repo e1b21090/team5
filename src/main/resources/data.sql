ad5745ec7f3f3d1817e3595fecba3fb98b7b9fa1
e1b21068 <e1b21068@oit.ac.jp>
2023-10-31 16:42:10 +0900

    データベースの作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO roles (name, use) VALUES ('市民', false);
+INSERT INTO roles (name, use) VALUES ('市民', false);
+INSERT INTO roles (name, use) VALUES ('人狼', false);
+INSERT INTO roles (name, use) VALUES ('人狼', false);
+INSERT INTO roles (name, use) VALUES ('占い師', false);
+INSERT INTO roles (name, use) VALUES ('怪盗', false);
+INSERT INTO userinfo (username, role_id) VALUES ('user1', null);
+INSERT INTO userinfo (username, role_id) VALUES ('user2', null);
+INSERT INTO userinfo (username, role_id) VALUES ('user3', null);
+INSERT INTO userinfo (username, role_id) VALUES ('user4', null);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE roles (
+    id IDENTITY,
+    name VARCHAR NOT NULL,
+    use BOOLEAN NOT NULL
+);
+
+CREATE TABLE userinfo (
+    id IDENTITY,
+    username VARCHAR NOT NULL,
+    role_id INTEGER
+);
