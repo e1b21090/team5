766242da123b38f8d113ecaa6cd3f3b6192ee472
Yuzuto Imai <e1b21012@st.oit.jp>
2024-01-09 15:09:16 +0900

    戻るボタン作成

--- src/main/java/team5/game/model/GamelogMapper.java
+++ src/main/java/team5/game/model/GamelogMapper.java
@@ -2,9 +2,15 @@ package team5.game.model;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import java.util.ArrayList;
 
 @Mapper
 public interface GamelogMapper {
-    @Insert("INSERT INTO gamelog (name1, role1, name2, role2, name3, role3, name4, role4, result) VALUES (#{name1}, #{role1}, #{name2}, #{role2}, #{name3}, #{role3}, #{name4}, #{role4}, #{result})")
-    void insertGamelog(Gamelog gamelog);
+  @Insert("INSERT INTO gamelog (name1, role1, name2, role2, name3, role3, name4, role4, result) VALUES (#{name1}, #{role1}, #{name2}, #{role2}, #{name3}, #{role3}, #{name4}, #{role4}, #{result})")
+  void insertGamelog(Gamelog gamelog);
+
+  @Select("SELECT * FROM gamelog ORDER BY id DESC LIMIT 5")
+  ArrayList<Gamelog> selectGamelog();
+
 }
